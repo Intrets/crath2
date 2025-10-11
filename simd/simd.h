f33808e42b663a151159878323b2b3ff0d91b7eb
Intrets <azarintrets@live.nl>
2025-10-11 02:53:39 +0200

    simple spacializer

--- simd/simd.h
+++ simd/simd.h
@@ -12,12 +12,14 @@ namespace cr
 #ifdef SIMD_8
 	using simd_1x = cr::simd::float1x8;
 	using simd_2x = cr::simd::float2x8;
+	using simd_8 = cr::simd::float1x8;
 	using simd_16 = cr::simd::float2x8;
 	using simd_type_or_fallback_scalar_1x = simd_1x;
 	using simd_type_or_fallback_scalar_2x = simd_2x;
 #elifdef SIMD_4
 	using simd_1x = cr::simd::float1x4;
 	using simd_2x = cr::simd::float2x4;
+	using simd_8 = cr::simd::float2x4;
 	using simd_16 = cr::simd::float4x4;
 	using simd_type_or_fallback_scalar_1x = simd_1x;
 	using simd_type_or_fallback_scalar_2x = simd_2x;
